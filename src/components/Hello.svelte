2a2c90b721da57074496ac524889c66a20650829
Romulo Mourão <romulo.dam@gmail.com>
2021-10-14 16:02:28 -0300

    first commit

---
+++ src/components/Hello.svelte
@@ -0,0 +1,12 @@
+<script>
+  import logo from "./logo.png";
+  export let title;
+</script>
+
+<div>
+  <img src={logo} class="mb-16 w-96 mx-auto" />
+
+  <h1 class="bg-white py-4 px-8 rounded-lg shadow-md text-gray-600 text-3xl text-center">
+    {title}
+  </h1>
+</div>
